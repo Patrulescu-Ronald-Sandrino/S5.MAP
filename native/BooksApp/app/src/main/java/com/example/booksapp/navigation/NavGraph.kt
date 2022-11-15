2c94cae2c814dca97265c1b0b34a017050dadb4e
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-15 18:56:21 +0200

    add routes for view book & edit book

--- native/BooksApp/app/src/main/java/com/example/booksapp/navigation/NavGraph.kt
+++ native/BooksApp/app/src/main/java/com/example/booksapp/navigation/NavGraph.kt
@@ -1,9 +1,12 @@
 package com.example.booksapp.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType.Companion.IntType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.booksapp.view.AddBookScreen
 import com.example.booksapp.view.BooksScreen
 
@@ -28,6 +31,23 @@ fun NavGraph(navController: NavHostController) {
             )
         }
 
-        // TODO: add other screens
+        composable(
+            route = "${Screen.ViewBook.route}/{id}",
+            arguments = listOf(navArgument("id") { type = IntType })
+        ) {
+            // TODO: implement view book screen
+            Text("View book screen")
+        }
+
+        composable(
+            route = "${Screen.UpdateBook.route}/{id}",
+            arguments = listOf(navArgument("id") { type = IntType })
+        ) {
+            // TODO: implement update book screen
+            Text("Update book screen")
+        }
+
+
+        // TODO: confirm dialog for delete book
     }
 }
--- native/BooksApp/app/src/main/java/com/example/booksapp/view/Texts.kt
+++ native/BooksApp/app/src/main/java/com/example/booksapp/view/Texts.kt
@@ -4,7 +4,7 @@ class Texts {
     companion object {
         const val BOOKS_LIST_SCREEN = "Books"
         const val ADD_BOOK_SCREEN = "Add book"
-        const val UPDATE_BOOK_SCREEN = "Update book"
         const val VIEW_BOOK_SCREEN = "View book"
+        const val UPDATE_BOOK_SCREEN = "Update book"
     }
 }
