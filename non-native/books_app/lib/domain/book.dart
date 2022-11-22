3f07bab724589c2213717c2cab45a66d394987c5
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2022-11-22 14:56:45 +0200

    [lab3] set up books screen

---
+++ non-native/books_app/lib/domain/book.dart
@@ -0,0 +1,11 @@
+
+
+class Book {
+  final int id;
+  final String title;
+  final String author;
+  final int year;
+  final bool lent;
+
+  Book(this.id, this.title, this.author, this.year, this.lent);
+}
