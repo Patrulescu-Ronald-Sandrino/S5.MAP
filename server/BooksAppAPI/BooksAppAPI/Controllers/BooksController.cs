2d8254607a55ae4e3c75a7666e7176295cffb479
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2023-01-15 21:11:15 +0200

    implement book endpoints

--- server/BooksAppAPI/BooksAppAPI/Controllers/BooksController.cs
+++ server/BooksAppAPI/BooksAppAPI/Controllers/BooksController.cs
@@ -26,6 +26,60 @@ public class BooksController : ControllerBase
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Book>>> Get()
     {
+        _logger.LogInformation($"[GET] Getting all books");
         return await _context.Books.ToListAsync();
     }
+    
+    
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Book>> Get(int id)
+    {
+        _logger.LogInformation("[GET] Looking for book with id {id}", id);
+        var book = await _context.Books.FindAsync(id);
+        if (book == null)
+        {
+            _logger.LogInformation("[GET] Book with id {id} not found", id);
+            return NotFound();
+        }
+        _logger.LogInformation("[GET] Found book with id {id}", id);
+        return book;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<Book>> Post(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Book with id {id} created", book.Id);
+        return CreatedAtAction(nameof(Get), new { id = book.Id }, book);
+    }
+    
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> Put(int id, Book book)
+    {
+        if (id != book.Id)
+        {
+            return BadRequest("[PUT] Book id does not match");
+        }
+        _context.Entry(book).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("[PUT] Book with id {id} updated", id);
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+        if (book == null)
+        {
+            _logger.LogInformation("[DELETE] Book with id {id} not found", id);
+            return NotFound();
+        }
+        _context.Books.Remove(book);
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("[DELETE] Book with id {id} deleted", id);
+        return NoContent();
+    }
+    
 }
--- server/BooksAppAPI/BooksAppAPI/Models/Book.cs
+++ server/BooksAppAPI/BooksAppAPI/Models/Book.cs
@@ -10,3 +10,12 @@ public class Book
     public int Year { get; set; }
     public bool Lent { get; set; }
 }
+
+public class PartialBook
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public int? Year { get; set; }
+    public bool? Lent { get; set; }
+}
